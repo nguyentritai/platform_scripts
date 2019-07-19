3f13e66d9c4f6af4a33eedb96a60ed1ee140b6ad
Tai Nguyen <ttnguyen@amperecomputing.com>
2019-07-19 16:22:15 -0700

    Update QS address steering spec change
    Signed-off-by: Tai Nguyen <ttnguyen@amperecomputing.com>

--- qs_addressing/addr_routing.py
+++ qs_addressing/addr_routing.py
@@ -28,9 +28,59 @@ col_bits=10
 row_bits=16
 bank_bits=4
 rank_bits=1
-num_mc=2
 
+mcu_mask=0xFF
 bank_hashing=1
+
+'''
+DV: Mesh programming for HNF hashing
+Address HNF hashing <-------> HNFNodeid based on SNF mode or MCU mask
+
+ if (snf_mode == 1 || snf_mode == 2 || snf_mode == 4 || snf_mode==6 || snf_mode == 8 || snf_vector == 1 || snf_vector == 17 || snf_vector == 51 || snf_vector == 119 || snf_vector ==255) begin
+    case(select[3:0])
+      'd0  : return `HNF0_NID12;
+      'd1  : return `HNF2_NID140;
+      'd8  : return `HNF4_NID332;
+      'd9  : return `HNF6_NID460;
+      'd2  : return `HNF8_NID20;
+      'd3  : return `HNF10_NID148;
+      'd10 : return `HNF12_NID340;
+      'd11 : return `HNF14_NID468;
+      'd4  : return `HNF16_NID28;
+      'd5  : return `HNF18_NID156;
+      'd12 : return `HNF20_NID348;
+      'd13 : return `HNF22_NID476;
+      'd6  : return `HNF24_NID36;
+      'd7  : return `HNF26_NID164;
+      'd14 : return `HNF28_NID356;
+      'd15 : return `HNF30_NID484;
+      default: assert(0);
+    endcase
+  end
+  else if (snf_vector == 16 || snf_vector == 240 || snf_vector == 15) begin
+    case(select[3:0])
+      'd0  : return `HNF0_NID12;
+      'd1  : return `HNF2_NID140;
+      'd2  : return `HNF4_NID332;
+      'd3  : return `HNF6_NID460;
+      'd4  : return `HNF8_NID20;
+      'd5  : return `HNF10_NID148;
+      'd6  : return `HNF12_NID340;
+      'd7  : return `HNF14_NID468;
+      'd8  : return `HNF16_NID28;
+      'd9  : return `HNF18_NID156;
+      'd10 : return `HNF20_NID348;
+      'd11 : return `HNF22_NID476;
+      'd12 : return `HNF24_NID36;
+      'd13 : return `HNF26_NID164;
+      'd14 : return `HNF28_NID356;
+      'd15 : return `HNF30_NID484;
+      default: assert(0);
+    endcase
+  end
+
+
+'''
 
 '''
 QS has 32 HN-Fs (HW). The hash selects the HN-F, which then picks the closest SN-F/DMC (SW)
@@ -47,41 +97,40 @@ X, Y : cross point node x,y
 P: 0: left bottom corner, 1: right top corner
 Z: 0: left HNF, 1:right HNF
 
+Example of 8 SNFs and 32 HNFs
 Address 	Address Hash	HNF Selected [X,Y,P,Z]
-0x8000_0000	0b00001		[0,1,1,1]
-0x8000_0040	0b00000		[0,1,1,0]
-0x8000_0080	0b00011		[2,1,1,1]
-0x8000_00c0	0b00010		[2,1,1,0]
-0x8000_0100	0b00101		[5,1,1,1]
-0x8000_0140	0b00100		[5,1,1,0]
-0x8000_0180	0b00111		[7,1,1,1]
-0x8000_01c0	0b00110		[7,1,1,0]
-0x8000_0200	0b01001		[0,2,1,1]
-0x8000_0240	0b01000		[0,2,1,0]
-0x8000_0280	0b01011		[2,2,1,1]
-0x8000_02c0	0b01010		[2,2,1,0]
-0x8000_0300	0b01101		[5,2,1,1]
-0x8000_0340	0b01100		[5,2,1,0]
-0x8000_0380	0b01111		[7,2,1,1]
-0x8000_03c0	0b01110		[7,2,1,0]
-0x8000_0400	0b10001		[0,3,1,1]
-0x8000_0440	0b10000		[0,3,1,0]
-0x8000_0480	0b10011		[2,3,1,1]
-0x8000_04c0	0b10010		[2,3,1,0]
-0x8000_0500	0b10101		[5,3,1,1]
-0x8000_0540	0b10100		[5,3,1,0]
-0x8000_0580	0b10111		[7,3,1,1]
-0x8000_05c0	0b10110		[7,3,1,0]
-0x8000_0600	0b11001		[0,4,1,1]
-0x8000_0640	0b11000		[0,4,1,0]
-0x8000_0680	0b01011		[2,4,1,1]
-0x8000_06c0	0b01010		[2,4,1,0]
-0x8000_0700	0b01101		[5,4,1,1]
-0x8000_0740	0b01100		[5,4,1,0]
-0x8000_0780	0b11111		[7,4,1,1]
-0x8000_07c0	0b11110		[7,4,1,0]
-
-
+0x8000_0000	0b00001		[2,1,1,0]	[0,1,0]
+0x8000_0040	0b00000		[0,1,1,0]	[0,1,0]
+0x8000_0080	0b00011		[2,2,1,0]	[0,2,0]
+0x8000_00c0	0b00010		[0,2,1,0]	[0,2,0]
+0x8000_0100	0b00101		[2,3,1,0]	[0,3,0]
+0x8000_0140	0b00100		[0,3,1,0]	[0,3,0]
+0x8000_0180	0b00111		[2,4,1,0]	[0,4,0]
+0x8000_01c0	0b00110		[0,4,1,0]	[0,4,0]
+0x8000_0200	0b01001		[7,1,1,0]	[7,1,0]
+0x8000_0240	0b01000		[5,1,1,0]	[7,1,0]
+0x8000_0280	0b01011		[7,2,1,0]	[7,2,0]
+0x8000_02c0	0b01010		[5,2,1,0]	[7,2,0]
+0x8000_0300	0b01101		[7,3,1,0]	[7,3,0]
+0x8000_0340	0b01100		[5,3,1,0]	[7,3,0]
+0x8000_0380	0b01111		[7,4,1,0]	[7,4,0]
+0x8000_03c0	0b01110		[5,4,1,0]	[7,4,0]
+0x8000_0400	0b10001		[2,1,1,1]	[0,1,0]
+0x8000_0440	0b10000		[0,1,1,1]	[0,1,0]
+0x8000_0480	0b10011		[2,2,1,1]	[0,2,0]
+0x8000_04c0	0b10010		[0,2,1,1]	[0,2,0]
+0x8000_0500	0b10101		[2,3,1,1]	[0,3,0]
+0x8000_0540	0b10100		[0,3,1,1]	[0,3,0]
+0x8000_0580	0b10111		[2,4,1,1]	[0,4,0]
+0x8000_05c0	0b10110		[0,4,1,1]	[0,4,0]
+0x8000_0600	0b11001		[7,1,1,1]	[7,1,0]
+0x8000_0640	0b11000		[5,1,1,1]	[7,1,0]
+0x8000_0680	0b01011		[7,2,1,1]	[7,2,0]
+0x8000_06c0	0b01010		[5,2,1,1]	[7,2,0]
+0x8000_0700	0b01101		[7,3,1,1]	[7,3,0]
+0x8000_0740	0b01100		[5,3,1,1]	[7,3,0]
+0x8000_0780	0b11111		[7,4,1,1]	[7,4,0]
+0x8000_07c0	0b11110		[5,4,1,1]	[7,4,0]
 '''
 HNF32_SELECT = [
 #               48......40......32......24......16.......8.......0
@@ -93,10 +142,30 @@ HNF32_SELECT = [
                 0b001000010000100001000010000100001000010000000000, # select[4]
                ]
 
-hnf_pair_2_xy = {0: 0x01, 1: 0x21, 2: 0x51, 3: 0x71,
-                 4: 0x02, 5: 0x22, 6: 0x52, 7: 0x72,
-                 8: 0x03, 9: 0x23, 10: 0x53, 11: 0x73,
-                 12: 0x04, 13: 0x24, 14: 0x54, 15: 0x74} 
+'''
+Map HNF address hash value to HNFNODEID
+'''
+mcu_mask_snf_mode1 = [0x01, 0x02, 0x04, 0x08, 0x06, 0x11, 0x33, 0x77, 0xFF]
+hnf_addr_hash_2_xy_snf_mode1 = {0: 0x01, 1: 0x21, 2: 0x02, 3: 0x22,
+		                 4: 0x03, 5: 0x23, 6: 0x04, 7: 0x24,
+		                 8: 0x51, 9: 0x71, 10: 0x52, 11: 0x72,
+		                 12: 0x53, 13: 0x73, 14: 0x54, 15: 0x74} 
+
+mcu_mask_snf_mode2 = [0x10, 0xf0, 0x0f]
+hnf_addr_hash_2_xy_mode2 = {0: 0x01, 1: 0x21, 2: 0x51, 3: 0x71,
+	                 4: 0x02, 5: 0x22, 6: 0x52, 7: 0x72,
+	                 8: 0x03, 9: 0x23, 10: 0x53, 11: 0x73,
+	                 12: 0x04, 13: 0x24, 14: 0x54, 15: 0x74} 
+
+xy_2_hnfpair = {0x01: 0, 0x21: 1, 0x51: 2, 0x71: 3,
+		0x02: 4, 0x22: 5, 0x52: 6, 0x72: 7,
+		0x03: 8, 0x23: 9, 0x53: 10, 0x73: 11,
+		0x04: 12, 0x24: 13, 0x54: 14, 0x74: 15}
+
+def popcount(i):
+    i = i - ((i >> 1) & 0x55555555)
+    i = (i & 0x33333333) + ((i >> 2) & 0x33333333)
+    return (((i + (i >> 4) & 0xF0F0F0F) * 0x1010101) & 0xffffffff) >> 24
 
 # Recursively partition the bits and do a dict lookup
 xor_4b = {0: 0, 1: 1, 2: 1, 3: 0, 4: 1, 5: 0, 6: 0, 7: 1, 8: 1, 9: 0, 10: 0, 11: 1, 12: 0, 13: 1, 14: 1, 15: 0}
@@ -114,15 +183,25 @@ def steer(sa):
   Hash to the HN-F and then steer to the closet MCU. Should be able to compute the XOR of all bit selects at the same time
   as they are in 5-bit chunks (e.g. XOR [10:6], [15:11],... [47:43]). 
   '''
-  hnf = 0
+  addr_hash = 0
   for i in range(len(HNF32_SELECT)):
-      hnf |= xor_div(sa & HNF32_SELECT[i], 64) << i
-  print ('hnf ' + str(hnf))
-  hnf_pair = hnf >> 1 
-  cal = hnf & 0x1
-  xp = (hnf_pair_2_xy[hnf_pair] >> 4) & 0xF 
-  yp = hnf_pair_2_xy[hnf_pair] & 0xF
-  print ('hnf pair={hnf_pair:01d} cal={cal:01d} ({xp:01d},{yp:01d})'.format(**locals()))
+      addr_hash |= xor_div(sa & HNF32_SELECT[i], 64) << i
+  print ('addr hash=0b{addr_hash:05b} '.format(**locals()))
+  hnf_pair_select_id = addr_hash & 0xF 
+  cal = (addr_hash & 0x10) >> 4
+  if mcu_mask in mcu_mask_snf_mode1:
+    hnf_addr_hash_2_xy = hnf_addr_hash_2_xy_snf_mode1
+  elif mcu_mask in mcu_mask_snf_mode2:
+    hnf_addr_hash_2_xy = hnf_addr_hash_2_xy_snf_mode2
+  else:
+    raise MemgenError('Unsupported MCU mask ({mcu_mask:02x})!'.format(**locals()))
+
+  xy = hnf_addr_hash_2_xy[hnf_pair_select_id]
+  xp = (xy >> 4) & 0xF 
+  yp = xy & 0xF
+  hnf_pair_id = xy_2_hnfpair[xy] 
+  print ('hnf pair id={hnf_pair_id:01d} cal={cal:01d} ({xp:01d},{yp:01d})'.format(**locals()))
+  print ('HNF Selected: [x, y, port, cal_mode] = [{xp:01d}, {yp:01d}, 1, {cal:01d}]'.format(**locals()))
 
 def sa2ma(sa):
   """
@@ -155,18 +234,19 @@ def mc_addr_inf(ma):
   '''
   Hashing enabled && mcu > 1, strip out SA based on the number of MCUs using 16 HN-Fs:
   1 None
-  2 [9]
-  4 [9, 8]
+  2 [7]
+  4 [8, 7]
   8 [9, 8, 7]
   '''
+  num_mc = popcount(mcu_mask)
   if num_mc == 1:
     lnr_addr = ma
   elif num_mc == 2:
-    lnr_addr = (((ma >> 10)) << 9) | (ma & ((1 << 9) - 1))
+    lnr_addr = (((ma >> (7 + 1))) << 7) | (ma & ((1 << 7) - 1))
   elif num_mc == 4:
-    lnr_addr = (((ma >> 10)) << 8) | (ma & ((1 << 8) - 1))
+    lnr_addr = (((ma >> (8 + 1))) << 7) | (ma & ((1 << 7) - 1))
   elif num_mc == 8:
-    lnr_addr = (((ma >> 10)) << 7) | (ma & ((1 << 7) - 1))
+    lnr_addr = (((ma >> (9 + 1))) << 7) | (ma & ((1 << 7) - 1))
 
   print('lnr_addr (memory addr after shuttered)={lnr_addr:010x}'.format(**locals()))
 
@@ -236,12 +316,15 @@ def mc_addr_inf(ma):
   
 # Current support only DRAM address
 def main():
-  print ('Address routing')
+  print ('Address routing:')
+  print ('---------------------------------------')
+  print ('Configuration: mcu_mask={mcu_mask:02x} Bank hashing={bank_hashing:01d}'.format(mcu_mask=mcu_mask, bank_hashing=bank_hashing))
   sa = sys.argv[1]
   ma = sa2ma(int(sa, 16))
   print ('system addr: {sa:s} memory addr: {ma:010x}'.format(**locals()))
   mc_addr_inf(ma)
 
+  print ('')
   steer(int(sa, 16))
 
 '''
